da2babd7d194d7314719fa062e8fbaf5700a1fa9
Mark B <mpboddie@gmail.com>
2019-12-14 00:19:36 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Tipsy',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This is so batshit crazy, it might just work.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
