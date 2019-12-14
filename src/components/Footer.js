372d5a486626a7a068d3765b326991ac1671f21b
Mark B <mpboddie@gmail.com>
2019-12-14 00:11:25 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -74,13 +74,13 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <!--<a title="facebook" href="https://facebook.com">
+                {/*<a title="facebook" href="https://facebook.com">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>-->
+                </a>*/}
                 <a title="twitter" href="https://twitter.com">
                   <img
                     className="fas fa-lg"
@@ -89,7 +89,7 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <!--<a title="instagram" href="https://instagram.com">
+                {/*<a title="instagram" href="https://instagram.com">
                   <img
                     src={instagram}
                     alt="Instagram"
@@ -102,7 +102,7 @@ const Footer = class extends React.Component {
                     alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>-->
+                </a>*/}
               </div>
             </div>
           </div>
