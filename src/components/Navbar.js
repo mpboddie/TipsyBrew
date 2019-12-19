5851027b4a9780756b2ab8e21967988fb0936b66
Mark B <mpboddie@gmail.com>
2019-12-18 23:28:57 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -87,10 +87,7 @@ const Navbar = class extends React.Component {
                   <img src={github} alt="Github" />
                 </span>
               </a>*/}
-              <a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/XkFdpfL">
-                <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"></img>
-                <span style="margin-left:15px;font-size:28px !important;">Buy me a coffee</span>
-              </a>
+              
             </div>
           </div>
         </div>
