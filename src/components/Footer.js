3b78b077a937a123fee124539dc7562f2523b0e6
Mark B <mpboddie@gmail.com>
2020-12-04 00:05:58 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
+import icon from '../img/coffeeLogo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -15,7 +16,7 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
           <img
-            src={logo}
+            src={icon}
             alt="TipsyBrew"
             style={{ width: '14em', height: '10em' }}
           />
