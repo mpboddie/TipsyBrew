388a26478f30006c922ec9a68161a90bee370722
Mark B <mpboddie@gmail.com>
2022-10-10 22:59:29 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,9 +66,11 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+        trackingIds: [
+          process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none"
+        ]
       }
     },
     {
